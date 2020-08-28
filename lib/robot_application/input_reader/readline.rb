a8cc4846854fe349274683d83d8e32f8997f1083
Alan Skorkin <alan@skorks.com>
2020-08-28 15:40:39 +1000

    Add better input reader abstraction

---
+++ lib/robot_application/input_reader/readline.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require "readline"
+
+module RobotApplication
+  module InputReader
+    class Readline
+      include Enumerable
+
+      attr_reader :input_io
+
+      def initialize(input_io: $stdin)
+        @input_io = input_io
+      end
+
+      def each(&_block)
+        while (line = ::Readline.readline("> ", true))
+          yield(line.chomp)
+        end
+      end
+    end
+  end
+end
